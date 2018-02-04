894face8bb5c35889bfd04aac345ea2e2794abbc
Rajat Deepak Mathur <rajatdem@andrew.cmu.edu>
2018-02-04 04:37:39 -0500

    [rajatdem] ignoring compile file

--- apps/periodic/hotplug_disable.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-echo 0 > /sys/module/cpu_tegra3/parameters/auto_hotplug
-CPU_PATH=/sys/devices/system/cpu
-for cpu in 0 1 2 3; do echo 1 > $CPU_PATH/cpu$cpu/online; sleep 1; done
-for cpu in 0 1 2 3; do echo performance > $CPU_PATH/cpu$cpu/cpufreq/scaling_governor; done
